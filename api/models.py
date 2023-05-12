87bc55a018ac54930f9061dc7808a8b546322b5f
fan9704 <cxz123499@gmail.com>
2023-05-12 15:18:26 +0800

    Upload Basic App

---
+++ api/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class TodoItem(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
